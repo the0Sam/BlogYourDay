c2344e57b0cdca4fd7e97db86e376b64c74c7e08
Sam <thesammu09@gmail.com>
2026-06-12 15:07:22 -0400

    commit

--- backend/server.js
+++ backend/server.js
@@ -23,5 +23,5 @@ app.use('/api/users', userRoutes);
 app.use('/api/vlogs', vlogRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on ${PORT}`);
 });
